ead208839641f3343e132b864937867606169794
thirdcoder <boris3rd@bk.ru>
2015-07-09 00:06:48 -0700

    Add tests for limits of 5-trit trytes (QZ to DM)

--- test.js
+++ test.js
@@ -8,6 +8,11 @@ test('sv2bts', function(t) {
   t.equal(sv2bts('Z0'), 'iii000');
   t.equal(sv2bts('ZA'), 'iii001');
   t.equal(sv2bts('MI'), '111100');
+  t.end();
+});
 
+test('5-trit limits', function(t) {
+  t.equal(sv2bts('QZ'), '0iiiii'); // might remove leading zeros here?
+  t.equal(sv2bts('DM'), '011111');
   t.end();
 });
